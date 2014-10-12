15756c4b5ffe9eb43f769faea3015f0f033f39fd
LanderlYoung <landerlyoung@gmail.com>
2014-10-12 12:59:26 +0800

    progress in util

---
+++ util/com/young/format/test/Main.java
@@ -0,0 +1,12 @@
+package com.young.format.test;
+
+/**
+ * Author: young
+ * Date:   2014-10-11
+ * Time:   下午10:57
+ * Life with passion. Code with creativity!
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
